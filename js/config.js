80e876cdc23062bc682e52b106c45374a9be3161
BertDarwinkel <57324035+BertDarwinkel@users.noreply.github.com>
2024-07-02 08:14:17 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ let respecConfig = {
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Basisregistratie Ondergrond Catalogus Milieuhygiënisch bodemonderzoek",
-  //subtitle: "Versie 1.1",
+  subtitle: "Versie 1.1",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   //specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
