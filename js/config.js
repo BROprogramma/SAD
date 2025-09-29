9d7d3629b5423280c0ef4767a073349e51ac5320
pasibun <matthijs_hovestad@hotmail.com>
2025-09-29 11:55:14 +0200

    Update specStatus and publishDate in config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ let respecConfig = {
   title: "Basisregistratie Ondergrond Catalogus Milieuhygiënisch bodemonderzoek",
   //subtitle: "Versie 1.1",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  specStatus: "cv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "sad",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2024-06-13",
+  publishDate: "2025-09-29",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
