87618891bb2afc47bef0fa523ec9818e61fc4956
Daoud Urdu <43752924+dwur@users.noreply.github.com>
2026-08-05 11:07:01 +0200

    terug naar werkversie van pre-release

--- js/config.js
+++ js/config.js
@@ -3,11 +3,11 @@ let respecConfig = {
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Basisregistratie Ondergrond Catalogus Milieuhygiënisch bodemonderzoek",
-  subtitle: "Versie 1.2",
+  //subtitle: "Versie 1.2",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  // specStatus: "wv",                   // Werkversie
+  specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
-  specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "sad",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2026-08-05",
+  //publishDate: "2026-08-05",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   publishVersion: " ",
