a3c38236d4e1aced4995af915a117730ceda0110
Daoud Urdu <43752924+dwur@users.noreply.github.com>
2026-08-05 10:02:15 +0200

    Modify respecConfig specStatus and publishDate
    tbv proefpublicatie

--- js/config.js
+++ js/config.js
@@ -5,9 +5,9 @@ let respecConfig = {
   title: "Basisregistratie Ondergrond Catalogus Milieuhygiënisch bodemonderzoek",
   //subtitle: "Versie 1.1",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  // specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
-  //specStatus: "vv",               // Versie ter vaststelling
+  specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "sad",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2025-07-22",
+  publishDate: "2026-08-05",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   publishVersion: " ",
